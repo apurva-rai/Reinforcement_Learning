d0c51710e2cacceac2c6c10679340ea22eaf59c9
Apurva <apurvarai99@gmail.com>
2021-05-17 00:07:49 -0500

    Implemented Q-Learning

---
+++ RL.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+#Q-Learning class that has functions to train and test simple treasure finding path
+class QL:
+
+    def __init__(self, a, r, action, reward, Q):
+        if a is None:
+            self.a = 0.5
+        if r is None:
+            self.r = 0.75
+
+        self.r = r
+        self.action = action
+        self.reward = reward
+        self.Q = Q
+
+    def trainer(self, i):
+        for j in range(i):
+            state = np.random.randint(0, int(len(self.Q)))
+            currentActions = []
+
+            for k in range(int(len(self.Q))):
+                if 0 <= self.reward[state,j]:
+                    currentActions.append(k)
+
+                nextState = int(np.random.choice(currentActions,1))
+                timeDiff = self.reward[state,nextState] + self.r * self.Q[nextState,np.argmax(self.Q[nextState,])] - self.Q[state,nextState]
+                self.Q[state,nextState] += self.a * timeDiff
+
+    def tester(self,start,end):
+        state = start
+        path = [state]
+
+        while state != end:
+            next = np.argmax(self.Q[state,])
+            path.append(next)
+            state = next
+
+        return path        
